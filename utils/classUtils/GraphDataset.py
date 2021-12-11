37163d011756833a8b844e000d10255f940cec20
Evan Koenig <ekoenig4@wisc.edu>
2021-12-11 17:00:26 -0600

    updated graph data and classifier

--- utils/classUtils/GraphDataset.py
+++ utils/classUtils/GraphDataset.py
@@ -21,18 +21,24 @@ class ScaleNodeAttrs:
     def transform(self, node_attrs):
         return (node_attrs - self.minims)/(self.maxims-self.minims)
 
+    def inverse(self, node_attrs):
+        return (self.maxims-self.minims)*node_attrs + self.minims
+
 
 class ScaleEdgeAttrs:
-    def __init__(self, invert=False):
-        if not invert:
-            self.transform = lambda x: (x-self.minim)/(self.maxim-self.minim)
-        else:
-            self.transform = lambda x: (self.maxim-x)/(self.maxim-self.minim)
-
-    def fit(self, edge_attrs):
-        self.minim = ak.min(edge_attrs)
-        self.maxim = ak.max(edge_attrs)
+    def fit(self, x):
+        self.nfeatures = x[0].to_numpy().shape[-1]
+        self.minims = ak.Array([ak.min(x[:, :, :, i])
+                               for i in range(self.nfeatures)])[None, None, None]
+        self.maxims = ak.Array([ak.max(x[:, :, :, i])
+                               for i in range(self.nfeatures)])[None, None, None]
         return self
+
+    def transform(self, x):
+        return (x - self.minims)/(self.maxims-self.minims)
+
+    def inverse(self, x):
+        return (self.maxims-self.minims)*x + self.minims
 
 
 def get_node_attrs(jets, attrs=["m", "pt", "eta", "phi", "btag"]):
@@ -46,8 +52,12 @@ def get_node_targs(jets):
     return targets
 
 
-def get_edge_attrs(jets, attrs=["dr"]):
-    return calc_dr(jets.eta[:, :, None], jets.phi[:, :, None], jets.eta[:, None], jets.phi[:, None])[:, :, :, None]
+def get_edge_attrs(jets, attrs=["dr", "dx"]):
+    dr = calc_dr(jets.eta[:, :, None], jets.phi[:, :, None],
+                 jets.eta[:, None], jets.phi[:, None])[:, :, :, None]
+    dx = np.sqrt((jets.x[:, None] - jets.x[:, :, None])**2 + (jets.y[:, None] -
+                 jets.y[:, :, None])**2 + (jets.z[:, None] - jets.z[:, :, None])**2)[:, :, :, None]
+    return ak.concatenate([dr, dx], axis=-1)
 
 
 def get_edge_targs(jets):
@@ -59,7 +69,13 @@ def get_edge_targs(jets):
     return 1*paired
 
 
-def build_features(tree, node_attr_names=["m", "pt", "eta", "phi", "btag"], edge_attr_names=["dr"]):
+def build_features(tree, node_attr_names=["m", "pt", "eta", "phi", "btag", "x", "y", "z"], edge_attr_names=["dr"]):
+    tree.extend(
+        jet_x=np.cos(tree.jet_phi),
+        jet_y=np.sin(tree.jet_phi),
+        jet_z=1/np.tan(2*np.arctan(np.exp(-tree.jet_eta)))
+    )
+
     jets = get_collection(tree, 'jet', named=False)
     node_attrs = get_node_attrs(jets, node_attr_names)
     node_targs = get_node_targs(jets)
@@ -69,7 +85,7 @@ def build_features(tree, node_attr_names=["m", "pt", "eta", "phi", "btag"], edge
     node_scaler = ScaleNodeAttrs().fit(node_attrs)
     node_attrs = node_scaler.transform(node_attrs)
 
-    edge_scaler = ScaleEdgeAttrs(invert=True).fit(edge_attrs)
+    edge_scaler = ScaleEdgeAttrs().fit(edge_attrs)
     edge_attrs = edge_scaler.transform(edge_attrs)
 
     node_class_weights = np.array(
@@ -85,7 +101,7 @@ def build_features(tree, node_attr_names=["m", "pt", "eta", "phi", "btag"], edge
     edge_features = ak.concatenate(
         [edge_attrs, edge_targs[:, :, :, None]], axis=-1)
 
-    return node_features, edge_features, node_class_weights, edge_class_weights, node_attr_names, edge_attr_names
+    return (node_features, node_scaler), (edge_features, edge_scaler), (node_class_weights, edge_class_weights), (node_attr_names, edge_attr_names)
 
 
 def build_graph(node_features, edge_features):
@@ -117,15 +133,17 @@ class Dataset(InMemoryDataset):
         self.node_class_weights, self.edge_class_weights = torch.load(
             self.processed_paths[2])
         self.filelist = torch.load(self.processed_paths[3])
+        self.node_scaler, self.edge_scaler = torch.load(
+            self.processed_paths[4])
 
     @property
     def processed_file_names(self):
-        return ['graphs.pt', 'attr_names.pt', 'class_weights.pt','filelist.pt']
+        return ['graphs.pt', 'attr_names.pt', 'class_weights.pt', 'filelist.pt', 'scalers.pt']
 
     def process(self):
         # Read data into huge `Data` list.
-        filelist = list(map(lambda f:f.fname,self.processed_tree.filelist))
-        node_features, edge_features, node_class_weights, edge_class_weights, node_attr_names, edge_attr_names = build_features(
+        filelist = list(map(lambda f: f.fname, self.processed_tree.filelist))
+        (node_features, node_scaler), (edge_features, edge_scaler), (node_class_weights, edge_class_weights), (node_attr_names, edge_attr_names) = build_features(
             self.processed_tree)
 
         data_list = build_dataset(node_features, edge_features)
@@ -142,4 +160,5 @@ class Dataset(InMemoryDataset):
         torch.save((node_attr_names, edge_attr_names), self.processed_paths[1])
         torch.save((node_class_weights, edge_class_weights),
                    self.processed_paths[2])
-        torch.save(filelist,self.processed_paths[3])
+        torch.save(filelist, self.processed_paths[3])
+        torch.save((node_scaler, edge_scaler), self.processed_paths[4])
