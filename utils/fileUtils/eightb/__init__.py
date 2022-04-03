c703a6145291508aaa24d85de573247f963ca787
Evan Koenig <ekoenig4@wisc.edu>
2022-04-03 11:48:55 -0500

    added eightb files

--- utils/fileUtils/eightb/__init__.py
+++ utils/fileUtils/eightb/__init__.py
@@ -4,5 +4,6 @@ from . import pt20_eta3_njet8
 from . import preselection
 from . import preselection_gnn
 from . import preselection_golden
+from . import preselection_golden_nobtag
 from . import training
 from . import preselection_passthrough
