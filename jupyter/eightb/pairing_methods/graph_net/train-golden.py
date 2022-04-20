dc933b2f29f402f09befd3efa63dbc937276ac61
Evan Koenig <ekoenig4@wisc.edu>
2022-04-20 14:32:34 -0500

    updating notebooks

--- jupyter/eightb/pairing_methods/graph_net/train-golden.py
+++ jupyter/eightb/pairing_methods/graph_net/train-golden.py
@@ -11,28 +11,35 @@ warnings.filterwarnings("ignore")
 import multiprocessing
 ncpu = multiprocessing.cpu_count()
 
+import torch
+torch.multiprocessing.set_sharing_strategy('file_system')
+
 
 from argparse import ArgumentParser 
 
 # --- ARGS --- #
 parser = ArgumentParser()
+
+parser.add_argument('--model',help='Select model to train',type=str,required=True,choices=gnn.modelMap.keys())
+parser.add_argument('--model-args',help='Specify model args',type=int,nargs='+',default=[])
+parser.add_argument('--output',help='Specify output directory for lightning logs',type=str, default='models')
+
 parser.add_argument('--epochs',help='Maximum number of epochs to run',type=int,default=100)
-parser.add_argument('--nn1_out',help='GoldenClassifier.nn1_out',type=int,default=32)
-parser.add_argument('--nn2_out',help='GoldenClassifier.nn2_out',type=int,default=128)
-parser.add_argument('--loss',help='Specify loss function to train with',type=str,default='std_loss',choices=gnn.losses.lossMap.keys())
+parser.add_argument('--loss',help='Specify loss function to train with',type=str,default='std_loss')
 parser.add_argument('--lr', help='Specify learning rate', type=float, default=1e-3)
 parser.add_argument('--scale',help='Specify how to scale features',type=str,default='standardize',choices=['raw','normalize','standardize'])
 parser.add_argument('--node-mask',help='Specify what node features to use',nargs='+',type=str,default=[])
 parser.add_argument('--edge-mask',help='Specify what edge features to use',nargs='+',type=str,default=[])
 
 parser.add_argument('--uptri',help='Use upper triangular adjacency matrix',default=False,action='store_true')
+parser.add_argument('--cluster-y',help='Calculate Cluster Y',default=False,action='store_true')
+parser.add_argument('--hyper-edge',help='Construct all possible 4 node hyper edges',default=False,action='store_true')
+parser.add_argument('--remove-self',help='Remove self loop from convolutions',default=False,action='store_true')
 
 # parser.add_argument('--train-size',help='Number of graphs to train with',type=int,default=-1) #! not implemented
 parser.add_argument('--valid-size',help='Fraction of training graphs to use as validation',type=float,default=0.2)
 parser.add_argument('--test-size',help='Fraction of testing graphs to use to test with',type=float,default=0.8)
 parser.add_argument('--batch-size',help='Specify batch size',type=int, default=1000)
-
-parser.add_argument('--model',help='Select model to train',type=str,default='golden',choices=gnn.modelMap.keys())
 
 parser.add_argument('--no-gpu',help='Dont use GPU',default=True,action='store_false')
 args = parser.parse_args()
@@ -44,12 +51,14 @@ gnn.config.set_gpu(args.no_gpu)
 print('Loading Training and Testing Data...')
 print('N CPU:',ncpu)
 
-transform = None
-if args.uptri: transform = gnn.to_uptri_graph()
+transform = gnn.Transform()
+if args.uptri: transform.append(gnn.to_uptri_graph())
+if args.cluster_y: transform.append(gnn.cluster_y())
+if args.hyper_edge: transform.append(gnn.HyperEdgeY())
+if args.remove_self: transform.append(gnn.remove_self_loops())
 
 template = gnn.Dataset('data/template',make_template=True, transform=transform, scale=args.scale, node_mask=args.node_mask, edge_mask=args.edge_mask)
-print(template.transform.transforms)
-exit()
+
 dataset = gnn.concat_dataset([f'data/{mass}-training' for mass in mass_list],transform=template.transform)
 
 testing = gnn.concat_dataset([f'data/{mass}-testing' for mass in mass_list],transform=template.transform)
@@ -66,9 +75,9 @@ testloader = DataLoader(testsample,batch_size=args.batch_size,num_workers=ncpu)
 
 # --- Loading Model --- #
 
-print('Loading GoldenGCN Model...')
+print('Loading GCN Model...')
 
-model = gnn.modelMap[args.model](template,nn1_out=args.nn1_out,nn2_out=args.nn2_out,loss=args.loss,lr=args.lr,batch_size=args.batch_size)
+model = gnn.modelMap[args.model](*args.model_args,dataset=template,loss=args.loss,lr=args.lr,batch_size=args.batch_size)
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
@@ -77,12 +86,12 @@ trainer = pl.Trainer(
     max_epochs=args.epochs,
     gpus=-1 if gnn.config.useGPU else 0,
     accelerator="auto",
-    precision=16,
+    # precision=16,
     callbacks=[EarlyStopping(
         monitor="hp/loss",
         stopping_threshold=1e-4,
         divergence_threshold=6.0)],
-    default_root_dir=model.output
+    default_root_dir=f'{args.output}/{model.name}'
 )
 
 # --- Fitting Model --- #
